895427ce7152489cbcbdc21f8a43e6f545d6c67e
vinhtruong204 <vt3839796@gmail.com>
2026-03-01 17:24:56 +0700

    feat: Add images for UI in lobby scene

---
+++ scripts/global_ui/global_ui.gd
@@ -0,0 +1,31 @@
+class_name GlobalUI extends CanvasLayer
+
+var scene_path := "res://scenes/main_container/lobby/lobby.tscn"
+@onready var progress_bar := $LoadingContainer/LoadingProgressBar
+var is_loaded := false
+
+func _ready():
+	ResourceLoader.load_threaded_request(scene_path)
+
+func _process(_delta):
+	if is_loaded:
+		return
+
+	var progress = []
+	var status := ResourceLoader.load_threaded_get_status(scene_path, progress)
+
+	match status:
+		ResourceLoader.THREAD_LOAD_IN_PROGRESS:
+			progress_bar.value = progress[0] * 100.0
+			print(progress[0])
+
+		ResourceLoader.THREAD_LOAD_LOADED:
+			progress_bar.value = progress[0] * 100.0
+			is_loaded = true
+
+			#await get_tree().create_timer(1).timeout # delay 1 second 
+			var packed_scene = ResourceLoader.load_threaded_get(scene_path)
+			var scene = packed_scene.instantiate()
+			get_parent().get_node("SceneContainer").add_child(scene)
+
+			$LoadingContainer.visible = false
