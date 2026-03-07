bee019c928f60460f497d20aa00addef75fc70eb
vinhtruong204 <vt3839796@gmail.com>
2026-03-07 23:13:05 +0700

    feat: handle global load scene, refactor get progress of loading screen

---
+++ scripts/global/scene_loader.gd
@@ -0,0 +1,42 @@
+extends Node
+
+var current_scene: Node = null
+var scene_path: String = ""
+var is_loaded := false
+var _progress := [0.0]
+
+func get_progress() -> float:
+	return _progress[0]
+
+func load_scene(path: String):
+	# Clean up old scene
+	if current_scene:
+		current_scene.queue_free()
+		current_scene = null
+		await get_tree().process_frame
+	
+	# Reset loaded variable
+	is_loaded = false
+	scene_path = path
+
+	# Load new scene
+	ResourceLoader.load_threaded_request(path)
+
+
+func _process(_delta):
+	if self.is_loaded:
+		return
+
+	var status := ResourceLoader.load_threaded_get_status(scene_path, _progress)
+
+	match status:
+		ResourceLoader.THREAD_LOAD_LOADED:
+			_progress = [1]
+			var packed_scene = ResourceLoader.load_threaded_get(scene_path)
+			
+			var scene = packed_scene.instantiate()
+			current_scene = scene
+			is_loaded = true
+
+			var container = get_tree().root.get_node("Main/SceneContainer")
+			container.add_child(scene)
--- scripts/global_ui/global_ui.gd
+++ scripts/global_ui/global_ui.gd
@@ -2,29 +2,20 @@ class_name GlobalUI extends CanvasLayer
 
 var scene_path := "res://scenes/main_container/lobby/lobby.tscn"
 @onready var progress_bar := $LoadingContainer/LoadingProgressBar
-var is_loaded := false
+var is_loaded
 
 func _ready():
-	ResourceLoader.load_threaded_request(scene_path)
+	SceneLoader.load_scene(scene_path)
+	is_loaded = false
 
 func _process(_delta):
 	if is_loaded:
 		return
 
-	var progress = []
-	var status := ResourceLoader.load_threaded_get_status(scene_path, progress)
+	var progress := SceneLoader.get_progress() * 100.0
+	progress_bar.value = progress
+	
+	if progress == 100.0:
+		$LoadingContainer.visible = false
+		is_loaded = true
 
-	match status:
-		ResourceLoader.THREAD_LOAD_IN_PROGRESS:
-			progress_bar.value = progress[0] * 100.0
-
-		ResourceLoader.THREAD_LOAD_LOADED:
-			progress_bar.value = progress[0] * 100.0
-			is_loaded = true
-
-			#await get_tree().create_timer(1).timeout # delay 1 second 
-			var packed_scene = ResourceLoader.load_threaded_get(scene_path)
-			var scene = packed_scene.instantiate()
-			get_parent().get_node("SceneContainer").add_child(scene)
-
-			$LoadingContainer.visible = false
