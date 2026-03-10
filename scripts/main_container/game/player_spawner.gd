398b10131d7940be7cdef7fe1a00fa32b0c83b77
vinhtruong204 <vt3839796@gmail.com>
2026-03-10 23:20:48 +0700

    feat: add joystick plugin

--- scripts/main_container/game/player_spawner.gd
+++ scripts/main_container/game/player_spawner.gd
@@ -5,16 +5,16 @@ var _players_list: Dictionary[int, Node] = {}
 
 func _ready():
 	spawn_function = _spawn_player
-
+	multiplayer.peer_connected.connect(_on_peer_connected)
+	
 	if multiplayer.is_server():
 		set_multiplayer_authority(multiplayer.get_unique_id())
-
-		multiplayer.peer_connected.connect(_on_peer_connected)
 		multiplayer.peer_disconnected.connect(_on_peer_disconnected)
 
 func _on_peer_connected(peer_id: int):
 	if not multiplayer.is_server(): return
 	
+	await get_tree().create_timer(1).timeout
 	var rng = RandomNumberGenerator.new()
 	self.spawn({"peer_id": peer_id, "pos": Vector2(rng.randf() * 300, rng.randf() * 300)})
 	
@@ -36,7 +36,6 @@ func _spawn_player(data: Dictionary) -> Node:
 	player.global_position = data["pos"]
 
 	player.set_multiplayer_authority(data["peer_id"])
-
 	_players_list[data["peer_id"]] = player
 	
 	return player
