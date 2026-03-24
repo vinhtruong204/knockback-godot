7a1905f88177cc40fa1d32d2c199ed7afd5e6f7c
vinhtruong204 <vt3839796@gmail.com>
2026-03-24 22:53:12 +0700

    feat: Implement player health, knockback, and bullet systems, including RPCs for damage and knockback.

---
+++ scripts/main_container/game/player/health/player_health.gd
@@ -0,0 +1,22 @@
+class_name PlayerHealth extends Node
+
+const MAX_HEALTH: int = 50
+const MAX_HEART: int = 2
+
+@export var health: int = MAX_HEALTH
+@export var heart: int = MAX_HEART
+
+@rpc("any_peer", "call_remote", "reliable")
+func take_damage_rpc(damage: int) -> void:
+	if not is_multiplayer_authority(): return
+
+	health -= damage
+
+	if health <= 0:
+		# Reset health and decrease heart
+		health = MAX_HEALTH
+		heart -= 1
+
+		if heart <= 0:
+			heart = 0
+			get_parent().visible = false # TODO: Add respawn system
--- scripts/main_container/game/player/knockback/player_knockback.gd
+++ scripts/main_container/game/player/knockback/player_knockback.gd
@@ -1,6 +1,7 @@
-class_name PlayerTakeDamage extends Node
+class_name PlayerKnockback extends Node
 
 @onready var player_movement: PlayerMovement = $"../PlayerMovement"
+@onready var player_health: PlayerHealth = $"../PlayerHealth"
 
 @rpc("any_peer", "call_remote", "reliable")
 func apply_knockback_rpc(direction: int, force: float):
