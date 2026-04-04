1e64f88fd5e3caad3b9dc8bf712200ff345bbe1a
vinhtruong204 <vt3839796@gmail.com>
2026-04-04 21:56:04 +0700

    feat: implement LAN, Normal lobby panel and network manager functionality

--- scripts/global_scripts/network_manager.gd
+++ scripts/global_scripts/network_manager.gd
@@ -2,9 +2,11 @@ extends Node
 
 const ADDRESS := "100.96.156.107"
 const PORT := 9543
+
 var game_scene_path := "res://scenes/main_container/game/game.tscn"
 var lobby_scene_path := "res://scenes/main_container/lobby/lobby.tscn"
 var login_scene_path := "res://scenes/main_container/login/login.tscn"
+
 
 func _ready():
 	if OS.has_feature("dedicated_server"):
@@ -20,28 +22,28 @@ func _ready():
 func _on_peer_connected(id):
 	print("Peer connected:", id)
 
+
 func _on_peer_disconnected(id):
 	print("Peer disconnected:", id)
 
-func create_client() -> void:
+
+func create_client(address: String = ADDRESS, port: int = PORT) -> void:
 	var peer := ENetMultiplayerPeer.new()
-	peer.create_client(ADDRESS, PORT)
+	
+	peer.create_client(address, port)
 	multiplayer.multiplayer_peer = peer
-
 	print("create client")
-
+	print("create client at " + address + ":" + str(port))
+	
 	SceneLoader.load_scene(game_scene_path)
 	get_tree().get_root().get_node("Main/GlobalUi").show_loading_screen()
 
-func create_server() -> void:
+func create_server(port: int = PORT) -> void:
 	var peer := ENetMultiplayerPeer.new()
-	peer.create_server(PORT)
+	peer.create_server(port)
 	multiplayer.multiplayer_peer = peer
-
-	print("start server at " + ADDRESS + ":" + str(PORT))
-
+	print("start server at port " + str(port))
 	SceneLoader.load_scene(game_scene_path)
-
 	multiplayer.peer_connected.connect(_on_peer_connected)
 	multiplayer.peer_disconnected.connect(_on_peer_disconnected)
 
